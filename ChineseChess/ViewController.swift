76ef8d9e166628362067ed499e9a23a91c98ee3d
Mulang Su <sumulang@gmail.com>
2021-01-23 08:59:00 +0800

    Initial Commit

---
+++ ChineseChess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChineseChess
+//
+//  Created by Mulang Su on 23/1/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
