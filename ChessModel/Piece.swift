69fdd3ad2076129f52c7974841ecac88ff616fb2
Mulang Su <sumulang@gmail.com>
2021-01-23 19:17:12 +0800

    added Player description

--- ChessModel/Piece.swift
+++ ChessModel/Piece.swift
@@ -2,4 +2,12 @@ enum Player : CustomStringConvertible{
     case red
     case black
     
+    var description: String {
+        switch self {
+        case .red:
+            return "Red"
+        case .black:
+            return "Black"
+        }
+    }
 }
