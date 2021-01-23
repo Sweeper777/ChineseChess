595f957bdc59c0c7b415c941e3113c2c55ff6f25
Mulang Su <sumulang@gmail.com>
2021-01-23 09:18:56 +0800

    added ChessModel target

---
+++ ChessModel/ChessModel.h
@@ -0,0 +1,18 @@
+//
+//  ChessModel.h
+//  ChessModel
+//
+//  Created by Mulang Su on 23/1/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChessModel.
+FOUNDATION_EXPORT double ChessModelVersionNumber;
+
+//! Project version string for ChessModel.
+FOUNDATION_EXPORT const unsigned char ChessModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChessModel/PublicHeader.h>
+
+
