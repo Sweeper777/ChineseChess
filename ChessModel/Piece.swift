265a72d6fb1198857b939eeaabc5c8591225f99e
Mulang Su <sumulang@gmail.com>
2021-01-24 15:39:45 +0800

    updated piece protocol

--- ChessModel/Piece.swift
+++ ChessModel/Piece.swift
@@ -1,7 +1,16 @@
-protocol Piece : CustomStringConvertible{
+public protocol Piece : CustomStringConvertible {
     var player: Player { get }
     var localisedDescription: String { get }
+    
+    /// precondition:
+    /// - all positions are in range
+    /// - move.to has a piece of the opposing player, or is empty
+    /// does not guarentee validation of king in check after the move
     func validateMove(_ move: Move, in board: Array2D<Piece?>) -> MoveError?
+    
+    /// postcondition:
+    /// - move.from == position
+    func allMoves(from position: Position, in board: Array2D<Piece?>) -> [Move]
     
 }
 
