2bd1b2352c45abe1ceeb9325406e5b7432c726a3
Mulang Su <sumulang@gmail.com>
2021-01-25 21:34:42 +0800

    added helper methods and properties for Game

--- ChessModel/Game.swift
+++ ChessModel/Game.swift
@@ -1,6 +1,29 @@
 public class Game {
     var board = Array2D<Piece?>(columns: 9, rows: 10, initialValue: nil)
     public var currentPlayer = Player.red
+    public var opposingPlayer: Player {
+        currentPlayer.opponent
+    }
+
+    public func allPositions(of player: Player) -> [Position] {
+        allPositions(of: player, in: board)
+    }
+    
+    public func kingPosition(of player: Player) -> Position {
+        kingPosition(of: player, in: board)
+    }
+    
+    func allPositions(of player: Player, in board: Array2D<Piece?>) -> [Position] {
+        board.indicesOf { $0?.player == player }
+    }
+    
+    func kingPosition(of player: Player, in board: Array2D<Piece?>) -> Position {
+        board.firstIndex(where: { $0 is King && $0?.player == player })!
+    }
+    
+    public func piece(at position: Position) -> Piece? {
+        board[position]
+    }
     
     public init() {
     }
