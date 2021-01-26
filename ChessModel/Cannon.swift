cbd33d62f2f06aa5d8bf2b05590f25eb153441ee
Mulang Su <sumulang@gmail.com>
2021-01-26 21:43:10 +0800

    added validateMove for Cannon

--- ChessModel/Cannon.swift
+++ ChessModel/Cannon.swift
@@ -15,6 +15,35 @@ class Cannon : Piece {
         }
     }
     
+    func validateMove(_ move: Move, in board: Array2D<Piece?>) -> MoveError? {
+        if abs(move.dx) > 0 && abs(move.dy) > 0 {
+            return .invalidPosition
+        }
+        
+        let numberOfPiecesInBetween: Int
+        if abs(move.dx) > 0 {
+            let start = min(move.from.x, move.to.x)
+            let end = max(move.from.x, move.to.x)
+            numberOfPiecesInBetween = ((start + 1)..<end).filter {
+                board[Position($0, move.from.y)] != nil
+            }.count
+        } else {
+            let start = min(move.from.y, move.to.y)
+            let end = max(move.from.y, move.to.y)
+            numberOfPiecesInBetween = ((start + 1)..<end).filter {
+                board[Position(move.from.x, $0)] != nil
+            }.count
+        }
+        if numberOfPiecesInBetween == 0 && board[move.to] == nil {
+            return nil
+        } else if numberOfPiecesInBetween == 1 && board[move.to]?.player == player.opponent {
+            return nil
+        } else {
+            return .invalidPosition
+        }
+        
+    }
+    
     
     init(_ player: Player) {
         self.player = player
