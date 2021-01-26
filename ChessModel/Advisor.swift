bb1c3e5bbd284a60595aa4fb952d17a9082483a0
Mulang Su <sumulang@gmail.com>
2021-01-26 21:42:30 +0800

    added validateMove for Advsior

--- ChessModel/Advisor.swift
+++ ChessModel/Advisor.swift
@@ -15,6 +15,38 @@ class Advisor : Piece {
         }
     }
     
+    func validateMove(_ move: Move, in board: Array2D<Piece?>) -> MoveError? {
+        let possibleEndLocations: [Position]
+        switch player {
+        case .black:
+            possibleEndLocations = [
+                Position(3, 0),
+                Position(5, 0),
+                Position(4, 1),
+                Position(3, 2),
+                Position(5, 2),
+            ]
+        case .red:
+            possibleEndLocations = [
+                Position(3, 7),
+                Position(5, 7),
+                Position(4, 8),
+                Position(3, 9),
+                Position(5, 9),
+            ]
+        }
+        
+        guard possibleEndLocations.contains(move.to) else {
+            return .invalidPosition
+        }
+        
+        guard abs(move.dx) == 1 && abs(move.dy) == 1 else {
+            return .invalidPosition
+        }
+        
+        return nil
+    }
+    
     
     init(_ player: Player) {
         self.player = player
