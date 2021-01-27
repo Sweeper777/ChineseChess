16ff4c5dd3a96f21cfad0f7045de35af58f84239
Mulang Su <sumulang@gmail.com>
2021-01-27 22:01:39 +0800

    added validateMove for Chariot

--- ChessModel/Chariot.swift
+++ ChessModel/Chariot.swift
@@ -15,5 +15,35 @@ class Chariot : Piece {
         }
     }
     
+    func validateMove(_ move: Move, in board: Array2D<Piece?>) -> MoveError? {
+        if abs(move.dx) > 0 && abs(move.dy) > 0 {
+            return .invalidPosition
+        }
+        
+        let blocked: Bool
+        if abs(move.dx) > 0 {
+            let start = min(move.from.x, move.to.x)
+            let end = max(move.from.x, move.to.x)
+            blocked = ((start + 1)..<end).contains(where: {
+                board[Position($0, move.from.y)] != nil
+            })
+        } else {
+            let start = min(move.from.y, move.to.y)
+            let end = max(move.from.y, move.to.y)
+            blocked = ((start + 1)..<end).contains(where: {
+                board[Position(move.from.x, $0)] != nil
+            })
+        }
+        if blocked {
+            return .invalidPosition
+        } else {
+            return nil
+        }
+    }
+    
+    
+    init(_ player: Player) {
+        self.player = player
+    }
 }
 
