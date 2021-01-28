069bb20588c639386aa8c6ba33287e94b1356c23
Mulang Su <sumulang@gmail.com>
2021-01-28 22:12:06 +0800

    added validateMove for Horse

--- ChessModel/Horse.swift
+++ ChessModel/Horse.swift
@@ -15,6 +15,39 @@ class Horse : Piece {
         }
     }
     
+    func validateMove(_ move: Move, in board: Array2D<Piece?>) -> MoveError? {
+        if abs(move.dx) == 2 {
+            guard abs(move.dy) == 1 else {
+                return .invalidPosition
+            }
+            if move.dx < 0 {
+                guard board[move.from.left()] == nil else {
+                    return .blocked
+                }
+            } else {
+                guard board[move.from.right()] == nil else {
+                    return .blocked
+                }
+            }
+            return nil
+        } else if abs(move.dy) == 2 {
+            guard abs(move.dx) == 1 else {
+                return .invalidPosition
+            }
+            if move.dy < 0 {
+                guard board[move.from.above()] == nil else {
+                    return .blocked
+                }
+            } else {
+                guard board[move.from.below()] == nil else {
+                    return .blocked
+                }
+            }
+            return nil
+        } else {
+            return .invalidPosition
+        }
+    }
     
     init(_ player: Player) {
         self.player = player
