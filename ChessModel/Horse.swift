8754a7da9ecbc7368117cca24ebf812c8d37987d
Mulang Su <sumulang@gmail.com>
2021-01-28 22:12:16 +0800

    added allMoves for Horse

--- ChessModel/Horse.swift
+++ ChessModel/Horse.swift
@@ -49,6 +49,32 @@ class Horse : Piece {
         }
     }
     
+    func allMoves(from position: Position, in board: Array2D<Piece?>) -> [Move] {
+        var endPositions = [Position]()
+        let above = position.above()
+        if (board[safe: above] as? Piece) == nil {
+            endPositions.append(above.above().left())
+            endPositions.append(above.above().right())
+        }
+        let below = position.below()
+        if (board[safe: below] as? Piece) == nil {
+            endPositions.append(below.below().left())
+            endPositions.append(below.below().right())
+        }
+        let left = position.left()
+        if (board[safe: left] as? Piece) == nil {
+            endPositions.append(left.left().above())
+            endPositions.append(left.left().below())
+        }
+        let right = position.right()
+        if (board[safe: right] as? Piece) == nil {
+            endPositions.append(right.right().above())
+            endPositions.append(right.right().below())
+        }
+        
+        return endPositions.map { Move(from: position, to: $0) }
+    }
+    
     init(_ player: Player) {
         self.player = player
     }
