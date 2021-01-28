f873c4cd815b05dcb626c776b9dcaf020a195938
Mulang Su <sumulang@gmail.com>
2021-01-28 22:11:28 +0800

    added validateMove for Elephant

--- ChessModel/Elephant.swift
+++ ChessModel/Elephant.swift
@@ -15,6 +15,47 @@ class Elephant : Piece {
         }
     }
     
+    func validateMove(_ move: Move, in board: Array2D<Piece?>) -> MoveError? {
+        let possibleEndLocations: [Position]
+        switch player {
+        case .black:
+            possibleEndLocations = [
+                Position(2, 0),
+                Position(6, 0),
+                Position(0, 2),
+                Position(2, 4),
+                Position(4, 2),
+                Position(8, 2),
+                Position(6, 4),
+            ]
+        case .red:
+            possibleEndLocations = [
+                Position(2, 9),
+                Position(6, 9),
+                Position(0, 7),
+                Position(2, 5),
+                Position(4, 7),
+                Position(8, 7),
+                Position(6, 5),
+            ]
+        }
+        
+        guard possibleEndLocations.contains(move.to) else {
+            return .invalidPosition
+        }
+        
+        guard abs(move.dx) == 2 && abs(move.dy) == 2 else {
+            return .invalidPosition
+        }
+        
+        let elephantsEye = Position((move.to.x + move.from.x) / 2, (move.to.y + move.from.y) / 2)
+        guard board[elephantsEye] == nil else {
+            return .blocked
+        }
+        
+        return nil
+    }
+    
     
     init(_ player: Player) {
         self.player = player
