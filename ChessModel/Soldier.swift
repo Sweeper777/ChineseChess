4c5a8ed48a63cf87b73838fd41c9e5f0bb0c33c6
Mulang Su <sumulang@gmail.com>
2021-01-29 09:17:41 +0800

    added validateMove for Soldier

--- ChessModel/Soldier.swift
+++ ChessModel/Soldier.swift
@@ -6,6 +6,35 @@ class Soldier : Piece {
         "\(player) Soldier"
     }
     
+    var localisedDescription: String {
+        switch player {
+        case .red:
+            return "兵"
+        case .black:
+            return "卒"
+        }
+    }
+    
+    func validateMove(_ move: Move, in board: Array2D<Piece?>) -> MoveError? {
+        let yBeforeRiver: ClosedRange<Int>
+        switch player {
+        case .red:
+            yBeforeRiver = 5...9
+        case .black:
+            yBeforeRiver = 0...4
+        }
+        if move.dy == -1 && move.dx == 0 && player == .red {
+            return nil
+        }
+        if move.dy == 1 && move.dx == 0 && player == .black {
+            return nil
+        }
+        if abs(move.dx) == 1 && move.dy == 0 && !yBeforeRiver.contains(move.from.y) {
+            return nil
+        }
+        return .invalidPosition
+    }
+    
     
     init(_ player: Player) {
         self.player = player
