e241cd4abfba1caf011f2d10ddf9c5953e05c2b8
Mulang Su <sumulang@gmail.com>
2021-01-31 11:51:22 +0800

    drew the black side of the board

--- ChineseChess/ChessBoardView.swift
+++ ChineseChess/ChessBoardView.swift
@@ -6,6 +6,24 @@ class ChessBoardView: UIView {
 
     
     override func draw(_ rect: CGRect) {
+        let squareSize = width / 9
+        let path = UIBezierPath()
+        let strokeWidth = squareSize / 20
+
+        func drawBlackSide() {
+            for x in 0..<8 {
+                for y in 0..<4 {
+                    let square = UIBezierPath(rect: CGRect(
+                            x: x.f * squareSize,
+                            y: y.f * squareSize,
+                            width: squareSize,
+                            height: squareSize
+                    ))
+                    path.append(square)
+                }
+            }
+        }
+
     }
 }
 
