c00715b66a54b1fca185f3ddea5c8a25442f3800
Mulang Su <sumulang@gmail.com>
2021-02-01 21:58:14 +0800

    added drawMarkingAt

--- ChineseChess/ChessBoardView.swift
+++ ChineseChess/ChessBoardView.swift
@@ -61,6 +61,37 @@ class ChessBoardView: UIView {
             path.append(palaces)
         }
         
+        func drawMarkingAt(x: Int, y: Int) -> UIBezierPath {
+            let markingOffset = squareSize / 10
+            let markingLength = squareSize / 5
+            let path = UIBezierPath()
+            if x < 8 {
+                // top right
+                path.move(to: CGPoint(x: x.f * squareSize + markingOffset, y: y.f * squareSize - markingOffset - markingLength))
+                path.addLine(to: CGPoint(x: x.f * squareSize + markingOffset, y: y.f * squareSize - markingOffset))
+                path.addLine(to: CGPoint(x: x.f * squareSize + markingOffset + markingLength, y: y.f * squareSize - markingOffset))
+                
+                // bottom right
+                path.move(to: CGPoint(x: x.f * squareSize + markingOffset, y: y.f * squareSize + markingOffset + markingLength))
+                path.addLine(to: CGPoint(x: x.f * squareSize + markingOffset, y: y.f * squareSize + markingOffset))
+                path.addLine(to: CGPoint(x: x.f * squareSize + markingOffset + markingLength, y: y.f * squareSize + markingOffset))
+            }
+            
+            if x > 0 {
+                // top left
+                path.move(to: CGPoint(x: x.f * squareSize - markingOffset, y: y.f * squareSize - markingOffset - markingLength))
+                path.addLine(to: CGPoint(x: x.f * squareSize - markingOffset, y: y.f * squareSize - markingOffset))
+                path.addLine(to: CGPoint(x: x.f * squareSize - markingOffset - markingLength, y: y.f * squareSize - markingOffset))
+                
+                // bottom left
+                path.move(to: CGPoint(x: x.f * squareSize - markingOffset, y: y.f * squareSize + markingOffset + markingLength))
+                path.addLine(to: CGPoint(x: x.f * squareSize - markingOffset, y: y.f * squareSize + markingOffset))
+                path.addLine(to: CGPoint(x: x.f * squareSize - markingOffset - markingLength, y: y.f * squareSize + markingOffset))
+            }
+            
+            return path
+        }
+
     }
 }
 
