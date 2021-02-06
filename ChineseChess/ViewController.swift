812844cf1e2b1126dea3e379a93bfa80e0f5c698
Mulang Su <sumulang@gmail.com>
2021-02-06 12:02:56 +0800

    deselected selected piece when tapped

--- ChineseChess/ViewController.swift
+++ ChineseChess/ViewController.swift
@@ -19,14 +19,17 @@ class ViewController: UIViewController {
 
 extension ViewController : ChessBoardViewDelegate {
     func didTapPosition(_ position: Position) {
-        if let tappedPiece = game.piece(at: position), tappedPiece.player == game.currentPlayer {
+        if position == chessBoardView.selectedPosition {
+            chessBoardView.selectedPosition = nil
+            chessBoardView.selectablePositions = []
+        } else if let tappedPiece = game.piece(at: position), tappedPiece.player == game.currentPlayer {
             chessBoardView.selectedPosition = position
             chessBoardView.selectablePositions =
                     tappedPiece.allMoves(from: position, in: game)
                             .filter { (try? game.validateMove($0)) != nil }
                             .map(\.to)
         } else if let startPosition = chessBoardView.selectedPosition,
-                  let selectedPiece = game.piece(at: startPosition) {
+                  let _ = game.piece(at: startPosition) {
             let move = Move(from: startPosition, to: position)
             do {
                 let moveResult = try game.makeMove(move)
