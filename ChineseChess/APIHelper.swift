9530679eb67d791abce1c89df78de2294c2a058a
Mulang Su <sumulang@gmail.com>
2021-02-07 11:07:24 +0800

    added parseResponseData

--- ChineseChess/APIHelper.swift
+++ ChineseChess/APIHelper.swift
@@ -12,3 +12,23 @@ enum APIError : Error {
     case invalidResponseFormat
 }
 
+
+fileprivate func parseResponseData(_ data: Data) -> Result<APIResult, Error> {
+    guard let responseString = String(data: data, encoding: .utf8) else {
+        return .failure(APIError.invalidEncoding)
+    }
+    if responseString.starts(with: "nobestmove") {
+        return .success(.noBestMove)
+    } else if responseString.starts(with: "invalid board") {
+        return .success(.invalidBoard)
+    } else {
+        guard let indexOfColon = responseString.firstIndex(of: ":") else {
+            return .failure(APIError.invalidResponseFormat)
+        }
+        let iccsMoveString = String(responseString[responseString.index(after: indexOfColon)...].prefix(4))
+        guard let move = Move(iccsString: iccsMoveString) else {
+            return .failure(APIError.invalidResponseFormat)
+        }
+        return .success(.move(move))
+    }
+}
