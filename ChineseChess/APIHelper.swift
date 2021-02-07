c697fd8c1c1459437dcd8902a3623f143a969add
Mulang Su <sumulang@gmail.com>
2021-02-07 11:07:34 +0800

    added requestNextMove

--- ChineseChess/APIHelper.swift
+++ ChineseChess/APIHelper.swift
@@ -12,6 +12,35 @@ enum APIError : Error {
     case invalidResponseFormat
 }
 
+func requestNextMove(game: Game, completion: @escaping (Result<APIResult, Error>) -> ()) {
+    guard var urlComponents = URLComponents(string: "https://www.chessdb.cn/chessdb.php") else {
+        completion(.failure(URLError(URLError.badURL)))
+        return
+    }
+    urlComponents.queryItems = [
+        URLQueryItem(name: "action", value: "querybest"),
+        URLQueryItem(name: "board", value: game.fenFormatString()),
+    ]
+
+    guard let url = urlComponents.url else {
+        completion(.failure(URLError(URLError.badURL)))
+        return
+    }
+    var request = URLRequest(url: url, timeoutInterval: Double.infinity)
+
+    request.httpMethod = "GET"
+
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data else {
+            completion(.failure(error!))
+            return
+        }
+
+        completion(parseResponseData(data))
+    }
+
+    task.resume()
+}
 
 fileprivate func parseResponseData(_ data: Data) -> Result<APIResult, Error> {
     guard let responseString = String(data: data, encoding: .utf8) else {
