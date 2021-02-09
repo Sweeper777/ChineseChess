078028564db75560c91b44bbc0cb6d54bafe594b
Mulang Su <sumulang@gmail.com>
2021-02-09 21:04:15 +0800

    added MessageView initialisers

--- ChineseChess/MessageView.swift
+++ ChineseChess/MessageView.swift
@@ -10,4 +10,16 @@ class MessageView : UIView {
         }
     }
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    func commonInit() {
+    }
 }
