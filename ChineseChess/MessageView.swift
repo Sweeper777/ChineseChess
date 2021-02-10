ef41ded87a61fe0bd130dce0b3214d72ffa0b1d9
Mulang Su <sumulang@gmail.com>
2021-02-10 16:06:00 +0800

    implemented MessageView.commonInit

--- ChineseChess/MessageView.swift
+++ ChineseChess/MessageView.swift
@@ -21,5 +21,21 @@ class MessageView : UIView {
     }
 
     func commonInit() {
+        layer.borderWidth = 3
+        layer.borderColor = UIColor.label.cgColor
+        backgroundColor = .systemBackground
+
+        label = UILabel(frame: bounds)
+        label.numberOfLines = 0
+
+        addSubview(label)
+
+        label.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(10)
+            make.right.equalToSuperview().offset(-10)
+            make.top.equalToSuperview().offset(10)
+            make.bottom.equalToSuperview().offset(-10)
+            make.width.lessThanOrEqualTo(300)
+        }
     }
 }
