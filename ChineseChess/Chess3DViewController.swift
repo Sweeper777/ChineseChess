e7c9f991b1a2286a93d2a713ddbd7593bb2d4210
Mulang Su <sumulang@gmail.com>
2021-02-21 10:59:18 +0800

    added Chess3DViewController

---
+++ ChineseChess/Chess3DViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+import SceneKit
+
+class Chess3DViewController : UIViewController {
+    @IBOutlet var sceneView: SCNView!
+
+    var scene: ChineseChessScene!
+
+    override func viewDidLoad() {
+        scene = ChineseChessScene()
+        scene.setup()
+        sceneView.scene = scene
+        sceneView.allowsCameraControl = true
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.backgroundColor = .black
+        sceneView.showsStatistics = true
+        sceneView.backgroundColor = .black
+    }
+}
