eaadb617420113b9a5a9b25c3c534316fa9e7a17
Mulang Su <sumulang@gmail.com>
2021-02-25 22:03:07 +0800

    added UIImage flipped extension

--- ChineseChess/3D/ChessPieceTextureGenerator.swift
+++ ChineseChess/3D/ChessPieceTextureGenerator.swift
@@ -6,3 +6,21 @@ enum ChessPieceTextureGenerator {
     private static var textureCache: [String: UIImage] = [:]
 
 }
+
+extension UIImage {
+    func flipImage() -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        let bitmap = UIGraphicsGetCurrentContext()!
+
+        bitmap.translateBy(x: size.width / 2, y: size.height / 2)
+        bitmap.scaleBy(x: -1.0, y: -1.0)
+
+        bitmap.translateBy(x: -size.width / 2, y: -size.height / 2)
+        bitmap.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return image
+    }
+}
