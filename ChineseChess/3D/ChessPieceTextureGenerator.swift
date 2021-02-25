33630fc1fb477cc0e83c61cfc2a3a0af751c253b
Mulang Su <sumulang@gmail.com>
2021-02-25 22:03:35 +0800

    added texture(for:)

--- ChineseChess/3D/ChessPieceTextureGenerator.swift
+++ ChineseChess/3D/ChessPieceTextureGenerator.swift
@@ -5,6 +5,35 @@ import SceneKit
 enum ChessPieceTextureGenerator {
     private static var textureCache: [String: UIImage] = [:]
 
+    static func texture(for chessPiece: Piece) -> UIImage {
+        if let cached = textureCache[chessPiece.description] {
+            return cached
+        }
+
+        let size = 500
+        let textSize = 400
+        let offset = (size - textSize) / 2
+
+        let fontSize = fontSizeThatFits(size: CGSize(width: textSize, height: textSize), text: chessPiece.localisedDescription as NSString, font: UIFont.systemFont(ofSize: 0))
+        UIGraphicsBeginImageContext(CGSize(width: size, height: size))
+        (chessPiece.player == .red ? UIColor(named: "redPieceBackground") : UIColor(named: "blackPieceBackground"))!
+            .setFill()
+        UIRectFill(CGRect(x: 0, y: 0, width: size, height: size))
+        let paraStyle = NSMutableParagraphStyle()
+        paraStyle.alignment = .center
+        (chessPiece.localisedDescription as NSString).draw(in: CGRect(x: offset, y: offset, width: textSize, height: textSize), withAttributes: [
+            .font: UIFont.systemFont(ofSize: fontSize),
+            .foregroundColor: UIColor.white,
+            .paragraphStyle: paraStyle
+        ])
+        let context = UIGraphicsGetCurrentContext()!
+        context.scaleBy(x: -1, y: 1)
+        context.translateBy(x: -size.f, y: 0)
+        let image = UIGraphicsGetImageFromCurrentImageContext()!.flipImage()!
+        textureCache[chessPiece.description] = image
+        return image
+    }
+
 }
 
 extension UIImage {
