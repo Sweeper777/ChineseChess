7eaf095b79fcff3f27954ff462bc55287e9a7684
Mulang Su <sumulang@gmail.com>
2021-02-25 22:05:38 +0800

    added all the chess piece nodes

--- ChineseChess/3D/ChineseChessScene.swift
+++ ChineseChess/3D/ChineseChessScene.swift
@@ -12,6 +12,18 @@ class ChineseChessScene: SCNScene {
         cameraNode.position.y = 10
         cameraNode.eulerAngles.x = -0.523599
         cameraNode.eulerAngles.y = -.pi / 2
+        for y in 0...9 {
+            for x in 0...8 {
+                if let piece = game.piece(at: Position(x, y)) {
+                    let geometry = SCNCylinder(radius: 0.4, height: 0.2)
+                    geometry.materials = ChessPieceTextureGenerator.materials(for: piece)
+                    let node = SCNNode(geometry: geometry)
+                    node.position = boardPosToScenePos(Position(x, y))
+                    node.name = "piece"
+                    rootNode.addChildNode(node)
+                }
+            }
+        }
     }
 }
 
