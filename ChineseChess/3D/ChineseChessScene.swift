b1f7c07ec159956b60c8789e86e593116ff2a9d3
Mulang Su <sumulang@gmail.com>
2021-02-25 22:04:52 +0800

    added conversion function that converts from/to board and scene coords

--- ChineseChess/3D/ChineseChessScene.swift
+++ ChineseChess/3D/ChineseChessScene.swift
@@ -10,3 +10,11 @@ class ChineseChessScene: SCNScene {
         cameraNode.eulerAngles.y = -.pi / 2
     }
 }
+
+func boardPosToScenePos(_ position: Position) -> SCNVector3 {
+    SCNVector3(x: 9 - Float(position.y), y: 0, z: Float(position.x))
+}
+
+func scenePosToBoardPos(_ position: SCNVector3) -> Position {
+    Position(Int(round(position.z)), 9 - Int(round(position.x)))
+}
