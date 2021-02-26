bb71ef5a0f0f26878ac33f6d2c8e5743171e0dac
Mulang Su <sumulang@gmail.com>
2021-02-26 21:06:57 +0800

    did a hit test on touchesEnded

--- ChineseChess/3D/Chess3DViewController.swift
+++ ChineseChess/3D/Chess3DViewController.swift
@@ -17,4 +17,17 @@ class Chess3DViewController : UIViewController {
         sceneView.backgroundColor = .white
         sceneView.pointOfView = scene.cameraNode
     }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+
+        if let point = touches.first?.location(in: sceneView) {
+            let results = sceneView.hitTest(point)
+            if let firstResult = results.first(where: { $0.node.name == "piece" }) {
+                let scenePos = firstResult.worldCoordinates
+                let boardPos = scenePosToBoardPos(scenePos)
+                scene.didTapBoardPos(boardPos, nodeTapped: firstResult.node)
+            }
+        }
+    }
 }
