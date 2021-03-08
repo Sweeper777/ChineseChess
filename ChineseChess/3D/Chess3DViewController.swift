20843175782fc3ae8aad16952956ec9ab788840a
Mulang Su <sumulang@gmail.com>
2021-03-08 13:53:43 +0800

    added rotate black pieces button

--- ChineseChess/3D/Chess3DViewController.swift
+++ ChineseChess/3D/Chess3DViewController.swift
@@ -47,9 +47,15 @@ class Chess3DViewController : UIViewController, ChessMessageDisplayer {
             UIAction(title: "查詢着法數據庫", state: doBlackAutoMoves ? .on : .off) { _ in
                 self.waitForChessDBMove()
             },
+            UIAction(title: "旋轉黑子", image: UIImage(systemName: "arrow.clockwise")) { _ in
+                guard !self.scene.isAnimating else {
+                    return
+                }
+                self.scene.rotateBlackPieces()
+            },
             UIAction(title: "返回主菜單", image: UIImage(systemName: "xmark")) { _ in
                 self.dismiss(animated: true)
-            }
+            },
         ])
     }
 
