27bb7c8549fc9b1a444cdc919a79670f3bc17a36
Mulang Su <sumulang@gmail.com>
2021-03-15 21:42:36 +0800

    added outlets for main menu buttons

--- ChineseChess/Common/MainMenuViewController.swift
+++ ChineseChess/Common/MainMenuViewController.swift
@@ -1,7 +1,11 @@
 import UIKit
+import SwiftyButton
 
 class MainMenuViewController : UIViewController {
 
+    @IBOutlet var twoDButton: PressableButton!
+    @IBOutlet var threeDButton: PressableButton!
+    
     @IBAction func twoDClick() {
         performSegue(withIdentifier: "show2D", sender: nil)
     }
