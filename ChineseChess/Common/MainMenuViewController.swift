f17e454dce7ba65804560893ca2a94e9a7c561a7
Mulang Su <sumulang@gmail.com>
2021-03-15 21:42:45 +0800

    set colours for menu buttons

--- ChineseChess/Common/MainMenuViewController.swift
+++ ChineseChess/Common/MainMenuViewController.swift
@@ -6,6 +6,11 @@ class MainMenuViewController : UIViewController {
     @IBOutlet var twoDButton: PressableButton!
     @IBOutlet var threeDButton: PressableButton!
     
+    override func viewDidLoad() {
+        twoDButton.colors = .init(button: .brown, shadow: UIColor.brown.darker())
+        threeDButton.colors = .init(button: UIColor.green.darker(), shadow: UIColor.green.darker().darker())
+    }
+    
     @IBAction func twoDClick() {
         performSegue(withIdentifier: "show2D", sender: nil)
     }
